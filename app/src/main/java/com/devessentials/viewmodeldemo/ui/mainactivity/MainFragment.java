942dc9d37ff92e4e50664d84f2bfdee8c3f9b540
Christos Christidis <christos.christidis86@gmail.com>
2019-05-09 17:48:57 +0300

    implemented 1-way and 2-way binding

--- app/src/main/java/com/devessentials/viewmodeldemo/ui/mainactivity/MainFragment.java
+++ app/src/main/java/com/devessentials/viewmodeldemo/ui/mainactivity/MainFragment.java
@@ -1,7 +1,7 @@
 package com.devessentials.viewmodeldemo.ui.mainactivity;
 
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,17 +9,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.devessentials.viewmodeldemo.R;
+import com.devessentials.viewmodeldemo.databinding.MainFragmentBinding;
+
+import static com.devessentials.viewmodeldemo.BR.viewModel;
 
 public class MainFragment extends Fragment {
 
-    private MainViewModel mViewModel;
-    private EditText mDollarText;
-    private TextView mResultText;
+    private MainFragmentBinding mBinding;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -29,47 +27,17 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false);
+
+        mBinding.setLifecycleOwner(this);
+
+        return mBinding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-
-        View view = getView();
-        if (view == null) return;
-        mDollarText = getView().findViewById(R.id.dollarText);
-        mResultText = getView().findViewById(R.id.resultText);
-        Button convertButton = getView().findViewById(R.id.convertButton);
-
-        final Observer<Float> resultObserver = new Observer<Float>() {
-            @Override
-            public void onChanged(@Nullable Float result) {
-                if (result != null) {
-                    mResultText.setText(result.toString());
-                }
-            }
-        };
-
-        // The LiveData observes the lifecycle of this fragment. resultObserver will be called when
-        // a) the data changes while the fragment is active and b) when the fragment goes from
-        // inactive to active (active = started or resumed).
-        mViewModel.getResult().observe(this, resultObserver);
-
-        convertButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mDollarText.getText().toString().equals("")) {
-                    mResultText.setText(R.string.no_value);
-                } else {
-                    mViewModel.setAmount(mDollarText.getText().toString());
-
-                    // SOS: No need for this. setAmount changes the LiveData, which in turn calls
-                    // resultObserver which updates the view...
-//                    mResultText.setText(mViewModel.getResult().toString());
-                }
-            }
-        });
+        MainViewModel myViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        mBinding.setVariable(viewModel, myViewModel);
     }
 }
--- app/src/main/java/com/devessentials/viewmodeldemo/ui/mainactivity/MainViewModel.java
+++ app/src/main/java/com/devessentials/viewmodeldemo/ui/mainactivity/MainViewModel.java
@@ -1,20 +1,38 @@
 package com.devessentials.viewmodeldemo.ui.mainactivity;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.util.Log;
 
-class MainViewModel extends ViewModel {
+public class MainViewModel extends ViewModel {
 
     private static final float usd_to_eu_rate = 0.75f;
-    private String mDollarText = "";
+    private String mDollarString = "";
     private final MutableLiveData<Float> mResult = new MutableLiveData<>();
 
-    void setAmount(String value) {
-        mDollarText = value;
-        mResult.setValue(Float.valueOf(mDollarText) * usd_to_eu_rate);
+    // SOS: both getDollarString and getResult are called when the layout is (re)created. But because
+    // getResult returns LiveData, it's also called whenever mResult is changed in-code! For this to
+    // work though, we must make the binding observe the lifecycle of the fragment (MainFragment.java)
+    public String getDollarString() {
+        Log.i("WTF", "getDollarString");
+        return mDollarString;
     }
 
-    MutableLiveData<Float> getResult() {
+    // SOS: mDollarString is 2-way bound, so changing the EditText calls setDollarString. This changes
+    // mResult, so getResult will also be called.
+    public void setDollarString(String s) {
+        Log.i("WTF", "setDollarString");
+        mDollarString = s;
+        if (mDollarString.equals("")) {
+            mResult.setValue(0f);
+        } else {
+            mResult.setValue(Float.valueOf(mDollarString) * usd_to_eu_rate);
+        }
+    }
+
+    public LiveData<Float> getResult() {
+        Log.i("WTF", "getResult");
         return mResult;
     }
 }
